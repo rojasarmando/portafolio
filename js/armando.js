83dec42259b84cf1abd3e871ace087943e6bebe6
Armando Rojas <rojasarmando2017@gmail.com>
2018-03-25 13:42:06 -0400

    funcion para cambiar de idioma

--- js/armando.js
+++ js/armando.js
@@ -1,25 +1,87 @@
 $(document).ready(function() {
 	
+	
+
+	localStorage['idioma'] = 'es'
+
 	// variable array para el texto
-	let texto = []
+	
+	let texto = [
+
+		{
+			'id' : '#head',
+			'es' : "¡Hola! mi nombre es Armando Rojas",
+			'en' : "¡Hi! mi name is Armando Rojas"
+		},
+		{
+			'id' : '#head2',
+			'es' : "¡Hola! mi nombre es Armando Rojas",
+			'en' : "¡Hi! mi name is Armando Rojas"
+		}
+	]
+
+
+
+	
 
  
 	// español = en
 	// ingles = es
 
-	texto['en']['head'] = "¡Hola! mi nombre es Armando Rojas"
-	texto['es']['head'] = "¡Hi! mi name is Armando Rojas"
-
-	texto['en']['botonIdioma'] = "Change to English"
-	texto['es']['botonIdioma'] = "Cambiar a Español"
-
-
 	/*Cambair el head*/
 
 
-	$("#botonIdioma").click(function(event) {
-		alert()
-	});
+	$("#botonIdioma").idiomas(texto)
+	
+
+	
 
 
 });
+
+
+
+(function($){
+
+	
+
+	$.fn.extend({
+		
+		idiomas: function(texto){
+
+			return this.each(function() {
+				$(this).click(function() {
+
+					if( localStorage['idioma'] == 'es' ) { 
+						
+						
+						localStorage.clear() 
+						localStorage['idioma'] = 'en' 
+						
+					}
+					else {
+
+						localStorage.clear() 
+						localStorage['idioma'] = 'es'
+	
+					}
+
+					for (let i = 0; i <texto.length; i++) 
+
+	   					$(texto[i].id).fadeOut('slow', () => {
+
+							$(texto[i].id)
+								.html(texto[0][localStorage.idioma])
+								.fadeIn('slow', () => {})
+						})
+
+   					
+  				 })
+			})
+		}
+
+		//------------------------------------
+
+
+	});
+})(jQuery);
