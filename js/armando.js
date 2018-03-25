e3adf3426b0887f5d51182478694e4a24a8533f5
Armando Rojas <rojasarmando2017@gmail.com>
2018-03-25 10:59:34 -0400

    mejorando el diseño

---
+++ js/armando.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	
+	// variable array para el texto
+	let texto = []
+
+ 
+	// español = en
+	// ingles = es
+
+	texto['en']['head'] = "¡Hola! mi nombre es Armando Rojas"
+	texto['es']['head'] = "¡Hi! mi name is Armando Rojas"
+
+	texto['en']['botonIdioma'] = "Change to English"
+	texto['es']['botonIdioma'] = "Cambiar a Español"
+
+
+	/*Cambair el head*/
+
+
+	$("#botonIdioma").click(function(event) {
+		alert()
+	});
+
+
+});
