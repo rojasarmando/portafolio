b608e4756c6c2f5d980853e9fc92052730397f49
Armando Rojas <rojasarmando2017@gmail.com>
2019-01-22 15:53:08 -0430

    menu banner

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-angular.module('portafolio',[]).controller('principal', ( $scope )=>{
+var app = angular.module('portafolio',[])
+
+app.controller('principal', ( $scope )=>{
 
  
     $scope.titulo = 'ejemplo'
+})
+
+
+app.controller('menu', ($scope) =>{
+
+
+
+    $scope.home = "Armando Rojas"; 
+
+    // Botonera 
+    $scope.botonera = [
+        {'titulo' : 'Conocimiento' , ref : '#services'   }, 
+        {'titulo' : 'Portafolio' , ref : '#services'   }, 
+        {'titulo' : 'Curriculum' , ref : '#services'   }, 
+        {'titulo' : 'Change to English'   }, 
+    ]
 })
