1c5cae64b3cbb6ce8367488cd9372cd50fff470e
MrRojas <rojasarmando260@gmail.com>
2021-09-26 22:50:56 -0400

    added navbar and header

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have as title 'Portafolio'`, () => {
+  it("should have as title 'Portafolio'", () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.componentInstance
     expect(app.title).toEqual('Portafolio')
