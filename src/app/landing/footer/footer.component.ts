068cebe962a232bae0b08397d066de4f04ca8204
Armando Rojas <rojasarmando2017@gmail.com>
2021-09-28 19:48:37 -0400

    added style portafolio

---
+++ src/app/landing/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  public urlWhatsApp = ''
+  private phoneNumber = '584143500985'
+
+  constructor() {
+    this.urlWhatsApp = `https://wa.me/${this.phoneNumber}?text=Buen%20día%20`
+  }
+
+  ngOnInit(): void {}
+}
