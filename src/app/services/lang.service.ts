a0a8a3f73ee3ec6390a975dee69f769a3e6c6103
MrRojas <rojasarmando260@gmail.com>
2021-09-28 21:28:40 -0400

    added translate

---
+++ src/app/services/lang.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+import { TranslateService } from '@ngx-translate/core'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LangService {
+  private activeLanguage = 'en'
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang(this.activeLanguage)
+  }
+  public toggleTranslate() {
+    this.activeLanguage = this.activeLanguage == 'es' ? 'en' : 'es'
+    this.translate.use(this.activeLanguage)
+  }
+}
