d192f95233c80d307778ee9c93dd6059eb111e7a
Armando Rojas <rojasarmando2017@gmail.com>
2021-10-06 19:08:25 -0400

    added animation btn header

--- src/app/landing/header/header.component.ts
+++ src/app/landing/header/header.component.ts
@@ -6,11 +6,25 @@ import { LangService } from 'src/app/services/lang.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  public animateBtn: Boolean = false;
+
   constructor(private langService: LangService) {}
 
   ngOnInit(): void {}
 
   public toggleLanguage() {
     this.langService.toggleTranslate();
+    this.addAnimation();
+  }
+
+  public addAnimation() {
+    this.animateBtn = false;
+    setTimeout(() => {
+      this.animateBtn = true;
+    }, 100);
+  }
+
+  public removeAnimation() {
+    this.animateBtn = false;
   }
 }
