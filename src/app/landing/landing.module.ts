65e89e063a63901bc050691447bd3e81a712d82c
= <=>
2021-10-06 18:29:21 -0400

    updated animation

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { LandingRoutingModule } from './landing-routing.module'
-import { NavbarComponent } from './navbar/navbar.component'
-import { LandingComponent } from './landing.component'
-import { HeaderComponent } from './header/header.component'
-import { PortafolioComponent } from './portafolio/portafolio.component'
-import { NgParticlesModule } from 'ng-particles'
-import { ParticlesComponent } from './particles/particles.component'
-import { FooterComponent } from './footer/footer.component'
-import { ContactComponent } from './contact/contact.component'
-import { AboutComponent } from './about/about.component'
-import { WorkComponent } from './work/work.component'
-import { SkillsComponent } from './skills/skills.component'
-import { TranslateModule } from '@ngx-translate/core'
+import { LandingRoutingModule } from './landing-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing.component';
+import { HeaderComponent } from './header/header.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { NgParticlesModule } from 'ng-particles';
+import { ParticlesComponent } from './particles/particles.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @NgModule({
   declarations: [
@@ -26,13 +27,14 @@ import { TranslateModule } from '@ngx-translate/core'
     ContactComponent,
     AboutComponent,
     WorkComponent,
-    SkillsComponent
+    SkillsComponent,
   ],
   imports: [
     CommonModule,
     LandingRoutingModule,
     NgParticlesModule,
-    TranslateModule
-  ]
+    TranslateModule,
+    NgxTypedJsModule,
+  ],
 })
 export class LandingModule {}
