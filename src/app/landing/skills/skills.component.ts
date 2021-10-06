d95e757d4c4a9aa76a217033584be3485e5acc41
Armando Rojas <rojasarmando2017@gmail.com>
2021-10-06 18:30:12 -0400

    added section skills

--- src/app/landing/skills/skills.component.ts
+++ src/app/landing/skills/skills.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { SkillInterface } from './skills.interface';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
-
+  public skills: Array<SkillInterface>;
+  constructor() {}
   ngOnInit(): void {
+    this.initSkills();
   }
-
+  private initSkills() {
+    this.skills = [
+      {
+        title: 'Laravel',
+        img: 'assets/img/laravel.svg',
+        description:
+          'Tengo experiencia en laravel 5.3, 5.8, Laravel 7 y Laravel 8',
+      },
+      {
+        title: 'Laravel',
+        img: 'assets/img/laravel.svg',
+      },
+      {
+        title: 'Laravel',
+        img: 'assets/img/laravel.svg',
+      },
+      {
+        title: 'Laravel',
+        img: 'assets/img/laravel.svg',
+      },
+    ];
+  }
 }
