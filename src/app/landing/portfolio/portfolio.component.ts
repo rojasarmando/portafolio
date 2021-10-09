47053ae4b295297cd6409a9021f9ece1795e7cdb
Armando Rojas <rojasarmando2017@gmail.com>
2021-10-09 00:50:23 -0400

    rename portfolio

---
+++ src/app/landing/portfolio/portfolio.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent implements OnInit {
+  public portfolios: Array<String> = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
