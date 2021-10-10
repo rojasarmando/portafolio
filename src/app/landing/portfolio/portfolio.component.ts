2648b7a4332be9b07d411c4ad5a46ae031d3697b
Armando Rojas <rojasarmando2017@gmail.com>
2021-10-09 20:10:22 -0400

    added portfolio

--- src/app/landing/portfolio/portfolio.component.ts
+++ src/app/landing/portfolio/portfolio.component.ts
@@ -6,8 +6,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-  public portfolios: Array<String> = [];
+  public portfolios: Array<any> = [];
+  public dataModal: any = {};
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initPortfolio();
+  }
+
+  initPortfolio(): void {
+    this.portfolios = [
+      {
+        title: 'Tienda Limon',
+        descrip: 'Ecommerce realizado con el stack MEAN',
+        url: 'https://www.youtube.com/watch?v=BgfcToAjfdc',
+        portada: 'assets/img/portfolio/1.jpg',
+        preview: [
+          {
+            url: 'assets/img/portfolio/1.jpg',
+            type: 'img'
+          },
+          {
+            url: 'https://www.youtube.com/embed/BgfcToAjfdc',
+            type: 'video-youtbe'
+          }
+        ]
+      },
+      {
+        title: 'Tienda Limon',
+        descrip: 'Ecommerce realizado con el stack MEAN',
+        url: 'https://www.youtube.com/watch?v=BgfcToAjfdc',
+        portada: 'assets/img/portfolio/1.jpg',
+        preview: [
+          {
+            url: 'assets/img/portfolio/1.jpg',
+            type: 'img'
+          },
+          {
+            url: 'https://www.youtube.com/embed/BgfcToAjfdc',
+            type: 'video-youtbe'
+          }
+        ]
+      },
+      {
+        title: 'Tienda Limon',
+        descrip: 'Ecommerce realizado con el stack MEAN',
+        url: 'https://www.youtube.com/watch?v=BgfcToAjfdc',
+        portada: 'assets/img/portfolio/1.jpg',
+        preview: [
+          {
+            url: 'assets/img/portfolio/1.jpg',
+            type: 'img'
+          },
+          {
+            url: 'https://www.youtube.com/embed/BgfcToAjfdc',
+            type: 'video-youtbe'
+          }
+        ]
+      }
+    ];
+  }
+
+  public openModal(id: number) {
+    this.dataModal = this.portfolios[id];
+
+    console.log(this.dataModal);
+  }
 }
