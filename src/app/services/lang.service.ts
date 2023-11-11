44238772c75643e8d782c872637b071bb7cf6686
rojasarmando <rojasarmando2017@gmail.com>
2023-11-11 18:46:00 -0400

    add service

---
+++ src/app/services/lang.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LangService {
+  private activeLanguage = environment.LANG;
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang(this.activeLanguage);
+  }
+  toggleTranslate() {
+    this.activeLanguage = this.activeLanguage == 'es' ? 'en' : 'es';
+    this.translate.use(this.activeLanguage);
+  }
+
+  getTranslate(key: string) {
+    return this.translate.instant(key);
+  }
+}
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { NgwWowModule } from 'ngx-wow';
 
 
-
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    NgwWowModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [HttpClient]
+      }
+    })
+
   ]
 })
 export class ShareModule { }
