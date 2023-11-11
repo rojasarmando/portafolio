06e55c20e430c236d8dd93f72ee2e520234f1f16
rojasarmando <rojasarmando2017@gmail.com>
2023-11-11 18:28:16 -0400

    add ignore .vscode

---
+++ src/app/share/share.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgwWowModule } from 'ngx-wow';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgwWowModule,
+  ]
+})
+export class ShareModule { }
