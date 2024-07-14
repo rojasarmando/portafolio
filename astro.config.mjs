760782fe9aaf9ccd1a06193295403b1c2005ed5b
Armando Rojas <34174895+rojasarmando@users.noreply.github.com>
2024-07-14 14:31:35 -0400

    add data jobs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind() , astroI18next()],
+  integrations: [tailwind()],
   markdown: {
     shikiConfig: {
       theme: 'material-theme',
